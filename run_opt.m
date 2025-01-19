e83282448f6ba69f84a6b37f4d21f0cad5df1ed4
Chengkai Zhu <63106657+Chengkai-Zhu@users.noreply.github.com>
2025-01-19 11:56:14 +0800

    Update and rename RandUHolevo.m to run_opt.m

--- run_opt.m
+++ run_opt.m
@@ -1,5 +1,5 @@
-function h = RandUHolevo(NK)
-s = size(NK{1});
+function h = run_opt(K)
+s = size(K{1});
 d = s(1);
 
 %% optimization
@@ -9,8 +9,8 @@ elem.p = sympositivedefinitesimplexfactory(1,num_p); % probability
 elem.psi = stiefelcomplexfactory(d, 1, num_p); % pure state ensemble
 
 problem.M = productmanifold(elem);
-problem.cost = @(X) Holevo_cost(X, NK, num_p);
-problem.egrad = @(X) struct('p', gradient_p(X, NK, num_p), 'psi', gradient_psi(X, NK, num_p)); % gradient
+problem.cost = @(X) Holevo_cost(X, K, num_p);
+problem.egrad = @(X) struct('p', gradient_p(X, K, num_p), 'psi', gradient_psi(X, K, num_p)); % gradient
 options.tolgradnorm = 1e-6;
 options.maxiter = 100000;
 
